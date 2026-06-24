23572f149212c05420607d73057b8d7f99b0e2f9
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-06-24 16:02:28 +0500

    a

--- src/test/java/com/appsdeveloperblog/app/ws/restassuredtest/TestCreateUser.java
+++ src/test/java/com/appsdeveloperblog/app/ws/restassuredtest/TestCreateUser.java
@@ -5,11 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import io.restassured.RestAssured;
+
 class TestCreateUser {
+	
+	private final String CONTEXT_PATH="/mobile-app-ws";
 
 	@BeforeEach
 	void setUp() throws Exception {
-	}
+		RestAssured.baseURI="/http://localhost";
+		RestAssured.port=8888;
+		}
 
 	@Test
 	void testCreateUser() {
