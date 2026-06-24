494b4db5071303d68ef807969f3bf51afd77d03e
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-06-24 15:12:59 +0500

    a

--- src/test/java/com/appsdeveloperblog/app/ws/restassuredtest/MobileAppWsRestAssuredTestApplicationTests.java
+++ src/test/java/com/appsdeveloperblog/app/ws/restassuredtest/MobileAppWsRestAssuredTestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.appsdeveloperblog.app.ws.restassuredtest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ src/test/java/com/appsdeveloperblog/app/ws/restassuredtest/TestCreateUser.java
@@ -0,0 +1,19 @@
+package com.appsdeveloperblog.app.ws.restassuredtest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestCreateUser {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testCreateUser() {
+		fail("Not yet implemented");
+	}
+
+}
