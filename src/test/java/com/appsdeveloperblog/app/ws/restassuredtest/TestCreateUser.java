a89091d97af95e17586bb6ee8f8371c9f9fcab35
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-06-25 10:47:15 +0500

    a

--- src/test/java/com/appsdeveloperblog/app/ws/restassuredtest/TestCreateUser.java
+++ src/test/java/com/appsdeveloperblog/app/ws/restassuredtest/TestCreateUser.java
@@ -1,25 +1,63 @@
 package com.appsdeveloperblog.app.ws.restassuredtest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 class TestCreateUser {
-	
-	private final String CONTEXT_PATH="/mobile-app-ws";
+
+	private final String CONTEXT_PATH = "/mobile-app-ws";
 
 	@BeforeEach
 	void setUp() throws Exception {
-		RestAssured.baseURI="/http://localhost";
-		RestAssured.port=8888;
-		}
-
-	@Test
-	void testCreateUser() {
-		fail("Not yet implemented");
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = 8888;
 	}
 
+	@Test
+	final void testCreateUser() {
+
+		List<Map<String, Object>> userAddresses = new ArrayList<>();
+
+		Map<String, Object> shippingAddress = new HashMap<>();
+		shippingAddress.put("city", "Vancouver");
+		shippingAddress.put("country", "Canada");
+		shippingAddress.put("streetName", "123 Street name");
+		shippingAddress.put("postalCode", "123456");
+		shippingAddress.put("type", "shipping");
+
+		userAddresses.add(shippingAddress);
+
+		Map<String, Object> userDetails = new HashMap<>();
+		userDetails.put("firstName", "Sergey");
+		userDetails.put("lastName", "Kargopolov");
+		userDetails.put("email", "sergey.kargopolov@swiftdeveloperblog.com");
+		userDetails.put("password", "123");
+		userDetails.put("addresses", userAddresses);
+
+		Response response = given()
+				.contentType("application/json")
+				.accept("application/json")
+				.body(userDetails)
+			.when()
+				.post(CONTEXT_PATH + "/users")
+			.then()
+				.statusCode(200)
+				.contentType("application/json")
+				.extract()
+				.response();
+
+		String userId = response.jsonPath().getString("userId");
+		assertNotNull(userId);
+	}
 }
